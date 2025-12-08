7fd64fe77bc96c289c0059a3c7f61eeeb041d8f8
adil meftoh <aameftoh@gmail.com>
2025-12-08 15:30:28 +0100

    result page with badge

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,7 +2,6 @@ class UserSession < ApplicationRecord
   belongs_to :user
   belongs_to :session
   belongs_to :group
-  # belongs_to :playlist
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -91,8 +91,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_08_085059) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "guest", default: false
     t.string "pseudo"
+    t.boolean "guest", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
