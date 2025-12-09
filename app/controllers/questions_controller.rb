2dc6270db39dbf7618612584ac081c303a82c086
Ludo de Bergeyck <ludo.bergeyck@gmail.com>
2025-12-09 09:46:25 +0100

    progress bar fonctionnel

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,25 +3,34 @@ require 'fuzzystringmatch'
 class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
-
   end
 
   def update
     @question = Question.find(params[:id])
 
     if @question.update(question_params)
-      jarow = FuzzyStringMatch::JaroWinkler.create( :native )
+      jarow = FuzzyStringMatch::JaroWinkler.create(:native)
       @accuracy_title = jarow.getDistance(@question.user_answer_title.downcase, @question.song.title.downcase)
       @accuracy_artist = jarow.getDistance(@question.user_answer_artist.downcase, @question.song.artist.downcase)
 
       @accuracy_artist >= 0.95 ? @question.successful_artist = 1 : @question.successful_artist = 0
       @accuracy_title >= 0.95 ? @question.successful_title = 1 : @question.successful_title = 0
       @question.save
-      render partial: "feedback_content", locals: {question: @question}
+
+      # Données pour la barre de progression
+      @questions = Question.where(session_id: @question.session_id).order(:id)
+      @total_questions = @questions.count
+      @current_index = @questions.find_index { |q| q.id == @question.id }.to_i + 1
+
+      # Répond aux deux formats possibles
+      respond_to do |format|
+        format.turbo_stream  # Utilise update.turbo_stream.erb
+        format.html { render partial: "feedback_content", locals: { question: @question } }
+      end
+
     else
       render :edit, status: :unprocessable_entity
     end
-
   end
 
   private
